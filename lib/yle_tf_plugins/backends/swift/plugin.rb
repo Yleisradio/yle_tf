fc1b65c77281926c45ceec0ee6a78dde96968365
Teemu Matilainen <teemu.matilainen@iki.fi>
2018-08-13 22:26:45 +0300

    Add support for Swift backend (#13)

---
+++ lib/yle_tf_plugins/backends/swift/plugin.rb
@@ -0,0 +1,16 @@
+require 'yle_tf'
+
+module YleTfPlugins
+  module Backends
+    module Swift
+      class Plugin < YleTf::Plugin
+        register
+
+        backend('swift') do
+          require_relative 'command'
+          Command
+        end
+      end
+    end
+  end
+end
