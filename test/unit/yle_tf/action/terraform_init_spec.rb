f0c42b09aab70fc53b48b360632e2495f1aa9d45
Teemu Matilainen <teemu.matilainen@iki.fi>
2020-01-24 12:36:06 +0200

    Symlink `errored.tfstate` from run dir to module dir
    Terraform stores state in `errored.tfstate` if it fails to persist a
    state to a backend. As YleTf runs in a temporary directory, it would be
    lost and recovery gets complicated.
    
    So make a symlink to the module dir to store an errored tfstate.

--- test/unit/yle_tf/action/terraform_init_spec.rb
+++ test/unit/yle_tf/action/terraform_init_spec.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+require 'pathname'
+require 'tmpdir'
+
 require 'yle_tf/action/terraform_init'
 require 'yle_tf/config'
 require 'yle_tf/system'
@@ -11,20 +15,33 @@ describe YleTf::Action::TerraformInit do
 
   describe '#call' do
     let(:env) { { config: config } }
-    let(:config) do
-      YleTf::Config.new(
-        'backend' => { 'type' => 'foo', 'foo' => {} }
-      )
-    end
+    let(:config) { instance_double('YleTf::Config') }
 
     let(:backend) { instance_double('YleTf::Backend') }
+
+    let(:module_dir) { Pathname.new(module_dir_path) }
+    let(:module_dir_path) { File.expand_path('../../fixtures/empty', __dir__) }
+
+    let(:errored_tfstate) { Pathname.new('errored.tfstate') }
 
     before do
       allow(action).to receive(:backend) { backend }
       allow(backend).to receive(:configure) { nil }
+      allow(config).to receive(:module_dir) { module_dir }
 
       allow(YleTf::System).to receive(:cmd)
       allow(YleTf::Logger).to receive(:info)
+
+      # Run in a tmpdir as a symlink will be created
+      @orig_pwd = Dir.pwd
+      @tmpdir = Dir.mktmpdir
+      Dir.chdir(@tmpdir)
+    end
+
+    after do
+      # Remove the tmpdir
+      Dir.chdir(@orig_pwd)
+      FileUtils.rm_rf(@tmpdir, secure: true)
     end
 
     it 'does not raise' do
@@ -44,6 +61,24 @@ describe YleTf::Action::TerraformInit do
     it 'initializes Terraform' do
       expect(YleTf::System).to receive(:cmd).with('terraform', 'init', any_args)
       action.call(env)
+    end
+
+    it 'creates symlink to errored.tfstate' do
+      action.call(env)
+      expect(errored_tfstate).to be_symlink
+      expect(errored_tfstate.readlink).to eq(module_dir.join('errored.tfstate'))
+    end
+
+    context 'when old errored.tfstate exists' do
+      before do
+        FileUtils.touch(errored_tfstate)
+      end
+
+      it 'replaces it with a symlink' do
+        action.call(env)
+        expect(errored_tfstate).to be_symlink
+        expect(errored_tfstate.readlink).to eq(module_dir.join('errored.tfstate'))
+      end
     end
   end
 end
